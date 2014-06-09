0bc25e835092132a87b65295fcdb9080baa34d4f
pjfl <github@roxsoft.co.uk>
2014-06-09 13:41:50 +0100

    0.2 Added strictures::defanged

--- lib/App/Doh.pm
+++ lib/App/Doh.pm
@@ -2,7 +2,7 @@ package App::Doh;
 
 use 5.010_001;
 use namespace::sweep;
-use version; our $VERSION = qv( sprintf '0.2.%d', q$Rev: 26 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.2.%d', q$Rev: 27 $ =~ /\d+/gmx );
 
 use Class::Usul::Constants  qw( TRUE );
 use Class::Usul::Types      qw( BaseType );
@@ -37,7 +37,7 @@ App::Doh - An easy way to document a project using Markdown
 
 =head1 Version
 
-This documents version v0.2.$Rev: 26 $ of L<App::Doh>
+This documents version v0.2.$Rev: 27 $ of L<App::Doh>
 
 =head1 Description
 
---
+++ lib/strictures/defanged.pm
@@ -0,0 +1,122 @@
+package strictures::defanged;
+
+use strict;
+use warnings;
+use version; our $VERSION = qv( sprintf '0.2.%d', q$Rev: 27 $ =~ /\d+/gmx );
+
+sub import {
+   require strictures; no warnings 'redefine';
+
+   $ENV{PERL_STRICTURES_ENABLED}
+      or *strictures::import = sub { strict->import; warnings->import };
+
+   return;
+}
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 Name
+
+strictures::defanged - Make strictures the same as just use strict warnings
+
+=head1 Synopsis
+
+   use strictures::defanged;
+   use strictures;
+
+=head1 Version
+
+This documents version v0.2.$Rev: 27 $ of L<strictures::defanged>
+
+=head1 Description
+
+Monkey patch the L<strictures> import method. Make it the same as just
+
+   use strict;
+   use warnings;
+
+This stops new warnings that appear in new versions of Perl5 from breaking
+working programs, which is what happens when warnings are made fatal
+
+L<Strictures|strictures> also has some heuristic involving version control
+directories that I don't care for so that gets patched out also
+
+The C<use strictures::defanged> line needs to appear before the
+C<use strictures> line. Once the C<strictures::defanged> import method
+has run the C<strictures> import method will have been replaced
+
+Using L<strictures::defanged> has the same program wide scope as L<strictures>.
+Once you've used it in your program B<every> package that uses L<strictures>
+will be using this instead
+
+If you put this in your script wrappers and not your package code then
+programs are unaffected by the additional constraints that C<strictures>
+imposes but the package code, and the tests, still get the "benefit"
+
+=head1 Configuration and Environment
+
+Setting the environment variable C<PERL_STRICTURES_ENABLED> to true
+skips the patching of the L<strictures> import subroutine and turns
+this module into a no-op
+
+=head1 Subroutines/Methods
+
+None
+
+=head1 Diagnostics
+
+None
+
+=head1 Dependencies
+
+=over 3
+
+=item L<strictures>
+
+=back
+
+=head1 Incompatibilities
+
+There are no known incompatibilities in this module
+
+=head1 Bugs and Limitations
+
+This module uses monkey patching, which is bad because it is action at a
+distance
+
+There are no known bugs in this module. Please report problems to
+http://rt.cpan.org/NoAuth/Bugs.html?Dist=strictures-defanged.
+Patches are welcome
+
+=head1 Acknowledgements
+
+Larry Wall - For the Perl programming language
+
+=head1 Author
+
+Peter Flanigan, C<< <pjfl@cpan.org> >>
+
+=head1 License and Copyright
+
+Copyright (c) 2014 Peter Flanigan. All rights reserved
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+=cut
+
+# Local Variables:
+# mode: perl
+# tab-width: 3
+# End:
+# vim: expandtab shiftwidth=3:
