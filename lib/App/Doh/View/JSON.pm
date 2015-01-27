da56629f55e84d3d72f6ae73683aa44e2d6eec40
pjfl <github@roxsoft.co.uk>
2015-01-27 00:40:44 +0000

    0.9 Scripts sources a local env script

--- lib/App/Doh/View/JSON.pm
+++ lib/App/Doh/View/JSON.pm
@@ -12,7 +12,7 @@ with q(App::Doh::Role::Component);
 with q(App::Doh::Role::Templates);
 
 # Public attributes
-has '+moniker'   => default => 'json';
+has '+moniker' => default => 'json';
 
 # Private attributes
 has '_transcoder' => is => 'lazy', isa => Object,
@@ -42,72 +42,6 @@ sub serialize {
 1;
 
 __END__
-
-=pod
-
-=encoding utf-8
-
-=head1 Name
-
-App::Doh::View::JSON - One-line description of the modules purpose
-
-=head1 Synopsis
-
-   use App::Doh::View::JSON;
-   # Brief but working code examples
-
-=head1 Description
-
-=head1 Configuration and Environment
-
-Defines the following attributes;
-
-=over 3
-
-=back
-
-=head1 Subroutines/Methods
-
-=head1 Diagnostics
-
-=head1 Dependencies
-
-=over 3
-
-=item L<Class::Usul>
-
-=back
-
-=head1 Incompatibilities
-
-There are no known incompatibilities in this module
-
-=head1 Bugs and Limitations
-
-There are no known bugs in this module. Please report problems to
-http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Doh.
-Patches are welcome
-
-=head1 Acknowledgements
-
-Larry Wall - For the Perl programming language
-
-=head1 Author
-
-Peter Flanigan, C<< <pjfl@cpan.org> >>
-
-=head1 License and Copyright
-
-Copyright (c) 2015 Peter Flanigan. All rights reserved
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself. See L<perlartistic>
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
-
-=cut
 
 # Local Variables:
 # mode: perl
